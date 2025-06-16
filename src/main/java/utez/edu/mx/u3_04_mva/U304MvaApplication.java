df37461a0ee8978243c8855bab1f8feb68a2951f
MarmolejoValle <20223tn132@utez.edu.mx>
2025-06-16 15:26:32 -0600

    start proyect

---
+++ src/main/java/utez/edu/mx/u3_04_mva/U304MvaApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.u3_04_mva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U304MvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U304MvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/u3_04_mva/U304MvaApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.u3_04_mva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U304MvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
