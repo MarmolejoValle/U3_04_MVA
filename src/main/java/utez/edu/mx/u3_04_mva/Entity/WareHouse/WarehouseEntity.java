b06d4b651839a8a04625e961cc6a62ba4273c89a
MarmolejoValle <20223tn132@utez.edu.mx>
2025-06-23 11:38:34 -0600

    add status in whorehouse

--- src/main/java/utez/edu/mx/u3_04_mva/Entity/WareHouse/WarehouseEntity.java
+++ src/main/java/utez/edu/mx/u3_04_mva/Entity/WareHouse/WarehouseEntity.java
@@ -23,7 +23,9 @@ public class WarehouseEntity {
     private String code;
     private LocalDate registrationDate;
 
+    @NotNull
     private double salePrice;
+    @NotNull
     private double rentalPrice;
 
     @Enumerated(EnumType.STRING)
@@ -31,14 +33,23 @@ public class WarehouseEntity {
 
     @ManyToOne
     private BranchEntity branch;
+    @ManyToOne(optional = false)
+    private ClientEntity owner;
     @ManyToOne
     private ClientEntity client;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private WarehouseStatus status;
 
     public enum Size {
         G, M, P
     }
-
-    @PrePersist
+    public enum WarehouseStatus {
+        DISPONIBLE,
+        RENTADO,
+        VENDIDO
+    }
     public void generateCode() {
         this.registrationDate = LocalDate.now();
         this.code = branch.getCode() + "-A" + id;
--- src/main/java/utez/edu/mx/u3_04_mva/Service/WareHouse/WarehouseServices.java
+++ src/main/java/utez/edu/mx/u3_04_mva/Service/WareHouse/WarehouseServices.java
@@ -1,25 +1,32 @@
 package utez.edu.mx.u3_04_mva.Service.WareHouse;
 
 import org.springframework.stereotype.Service;
+import utez.edu.mx.u3_04_mva.Entity.Branch.BranchRepository;
 import utez.edu.mx.u3_04_mva.Entity.WareHouse.WareHouseRepository;
 import utez.edu.mx.u3_04_mva.Entity.WareHouse.WarehouseEntity;
+import utez.edu.mx.u3_04_mva.Service.Branch.BranchServices;
 
 import java.util.List;
 
 @Service
 public class WarehouseServices {
     private final WareHouseRepository wareHouseRepository;
+    private final BranchRepository branchRepository;
 
 
-    public WarehouseServices(WareHouseRepository wareHouseRepository) {
+    public WarehouseServices(WareHouseRepository wareHouseRepository, BranchRepository branchRepository) {
         this.wareHouseRepository = wareHouseRepository;
+        this.branchRepository = branchRepository;
     }
     public List<WarehouseEntity> getWarehouses(){
         return wareHouseRepository.findAll();
     }
 
     public WarehouseEntity add(WarehouseEntity warehouseEntity){
-        return wareHouseRepository.save(warehouseEntity);
+        warehouseEntity.setBranch(branchRepository.findById(warehouseEntity.getBranch().getId()).get());
+        WarehouseEntity newWareHouse =  wareHouseRepository.save(warehouseEntity);
+        newWareHouse.generateCode();
+        return wareHouseRepository.save(newWareHouse);
     }
     public WarehouseEntity update(WarehouseEntity warehouseEntity){
         return wareHouseRepository.save(warehouseEntity);
